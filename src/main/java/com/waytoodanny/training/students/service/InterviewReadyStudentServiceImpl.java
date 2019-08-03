50597d1fa6a8614dca3a97f69ffbfd635c7123fd
Daniel Volnitsky <waytodanny@gmail.com>
2019-08-03 15:17:35 +0300

    add tricky method to students service impl

--- src/main/java/com/waytoodanny/training/students/service/InterviewReadyStudentServiceImpl.java
+++ src/main/java/com/waytoodanny/training/students/service/InterviewReadyStudentServiceImpl.java
@@ -3,6 +3,7 @@ package com.waytoodanny.training.students.service;
 import com.waytoodanny.training.students.domain.Student;
 import com.waytoodanny.training.students.domain.filters.CompanyStudentFilter;
 import com.waytoodanny.training.students.repository.CompanyStudentRepository;
+import lombok.SneakyThrows;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -20,18 +21,28 @@ public class InterviewReadyStudentServiceImpl implements InterviewReadyStudentSe
 
     @Override
     public Collection<Student> getAllReadyForInterview() {
-        Collection<Student> students = repository.getAll();
+        Collection<Student> all = repository.getAll();
         if (interviewReadyFilters.size() == 0) {
-            return students;
+            return all;
         }
 
-        return interviewReadyFilters.stream()
-                .map(f -> f.apply(students))
+        HashSet<Student> ready = interviewReadyFilters.stream()
+                .map(filter -> filter.apply(all))
                 .map(HashSet::new)
                 .reduce((s1, s2) -> new HashSet<Student>() {{
                     addAll(s1);
                     addAll(s2);
                 }})
                 .orElseGet(HashSet::new);
+
+        notifyRemoteServiceAboutGreatStudents(ready);
+        return ready;
+    }
+
+    //bad-designed method, but it is here for a reason!
+    @SneakyThrows
+    void notifyRemoteServiceAboutGreatStudents(Collection<Student> students) {
+        Thread.sleep(2000);
+        //imagine that it actually happens
     }
 }
