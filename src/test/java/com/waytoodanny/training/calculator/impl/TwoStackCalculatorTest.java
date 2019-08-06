4cfc2f99d5d7410e2146f6f804e25ef4d66a34ce
Oleksandr Roienko <Oleksandr_Roienko@epam.com>
2019-08-06 17:28:25 +0300

    Made test

--- src/test/java/com/waytoodanny/training/calculator/impl/TwoStackCalculatorTest.java
+++ src/test/java/com/waytoodanny/training/calculator/impl/TwoStackCalculatorTest.java
@@ -3,14 +3,71 @@ package com.waytoodanny.training.calculator.impl;
 
 import com.waytoodanny.training.calculator.util.CalculatorUtil;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 class TwoStackCalculatorTest {
 
     private final TwoStackCalculator calculator =
             new TwoStackCalculator(CalculatorUtil.BASIC_OPERATIONS);
 
-    @Test
-    void calculate() {
 
+    @ParameterizedTest
+    @CsvSource(value = {"2+2=4", "7+3=10", "100+450=550", "2+0+3+3=8", "0+0+0+0+10+45+11=66"}, delimiter = '=')
+    void shouldReturnResultOfPlusOperations(String input, String expected) {
+        assertEquals(Double.parseDouble(expected), calculator.calculate(input));
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2-2=0", "7-3=4", "100-10-150=-60", "0-0=0", "56-40=16"}, delimiter = '=')
+    void shouldReturnResultOfMinusOperations(String input, String expected) {
+        assertEquals(Double.parseDouble(expected), calculator.calculate(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2*2=4", "7*3=21", "5*5=25", "2*0=0", "10*10*10*10=10000"}, delimiter = '=')
+    void shouldReturnResultOfMultiplyOperations(String input, String expected) {
+        assertEquals(Double.parseDouble(expected), calculator.calculate(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2/2=1", "8/2/2=2", "10/2=5", "12/2=6", "998/1=998"}, delimiter = '=')
+    void shouldReturnResultOfDivideOperations(String input, String expected) {
+        assertEquals(Double.parseDouble(expected), calculator.calculate(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2=2", "5=5", "6=6", "99=99", "3=3"}, delimiter = '=')
+    void shouldReturnSameNumber(String input, String expected) {
+        assertEquals(Double.parseDouble(expected), calculator.calculate(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2+2/2=3", "2*2/2+1-1=2", "5-15+5*2+10/5=2", "18/2*4=36", "2*2+2/2+5-1=9"}, delimiter = '=')
+    void shouldReturnResultOfUsingAllOperationsWithoutParenthesis(String input, String expected) {
+        assertEquals(Double.parseDouble(expected), calculator.calculate(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2+2*(2+2)=10", "2*(9/(2+1))-1=5", "(5-15+5)*(2+10/5)=-20", "64/(2*4)=8", "(2*2+2)/2+5-1=7"}, delimiter = '=')
+    void shouldReturnResultOfUsingAllOperationsWithUsingParenthesis(String input, String expected) {
+        assertEquals(Double.parseDouble(expected), calculator.calculate(input));
+    }
+
+    @Test
+    void shouldThrowArithmeticException() {
+        assertThrows(ArithmeticException.class, () -> calculator.calculate("1/0"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a+b", "2+3a", "4*(a)", "2/a"})
+    void shouldThrowIllegalArgumentException(String input) {
+        assertThrows(IllegalArgumentException.class, () -> calculator.calculate(input));
+    }
+
+
 }
--- src/test/java/com/waytoodanny/training/students/service/InterviewReadyStudentServiceImplTest.java
+++ src/test/java/com/waytoodanny/training/students/service/InterviewReadyStudentServiceImplTest.java
@@ -1,17 +1,84 @@
 package com.waytoodanny.training.students.service;
 
+import com.waytoodanny.training.students.domain.Student;
+import com.waytoodanny.training.students.domain.filters.CompanyStudentFilter;
+import com.waytoodanny.training.students.domain.filters.PreLabSufficientMarkFilter;
+import com.waytoodanny.training.students.domain.filters.WithinLabSufficientMarkFilter;
+import com.waytoodanny.training.students.repository.CompanyStudentRepository;
+import com.waytoodanny.training.students.repository.CompanyStudentRepositoryImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.lang.reflect.Array;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
 
-import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class InterviewReadyStudentServiceImplTest {
 
+    @Mock
+    private CompanyStudentRepositoryImpl repository;
+
+    private Collection<CompanyStudentFilter> interviewReadyFilters = new ArrayList<>();
+
+    private InterviewReadyStudentServiceImpl interviewReadyStudentService;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        interviewReadyStudentService =
+                Mockito.spy(new InterviewReadyStudentServiceImpl(repository, interviewReadyFilters));
+    }
+
+
     @Test
-    void getAllReadyForInterview() {
+    void shouldReturnAllStudents(){
+        Mockito.when(repository.getAll()).thenReturn(new HashSet<>(Arrays.asList(
+                new Student("Masha", 5, 5),
+                new Student("Gena", 2, 3),
+                new Student("Vanya", 3, 4),
+                new Student("Sveta", 5, 5),
+                new Student("Bodya", 3, 3)
+        )));
+    }
+
+    @Test
+    void ShouldReturnAllStudentsWithoutAddingFilter() {
         assertTimeout(Duration.ofMillis(1000), () -> {
-            //write your test logic here
+            Collection<Student> result = interviewReadyStudentService.getAllReadyForInterview();
+            Collection<Student> expected = repository.getAll();
+            assertEquals(expected, result);
+            doNothing().when(interviewReadyStudentService).notifyRemoteServiceAboutGreatStudents(anyCollection());
+            verify(repository, times(2)).getAll();
+        });
+
+    }
+
+    @Test
+    void shouldReturnFilteredInterviewReadyStudents() {
+        interviewReadyFilters.add(new PreLabSufficientMarkFilter());
+        interviewReadyFilters.add(new WithinLabSufficientMarkFilter());
+
+        repository = new CompanyStudentRepositoryImpl();
+        interviewReadyStudentService = new InterviewReadyStudentServiceImpl(repository, interviewReadyFilters);
+
+        Collection<Student> result = interviewReadyStudentService.getAllReadyForInterview();
+        assertTimeout(Duration.ofMillis(1000), () -> {
+            Collection<Student> expected =  Arrays.asList(
+                    new Student("Sveta", 5, 5),
+                    new Student("Vanya", 3, 4),
+                    new Student("Masha", 5, 5)
+            );
+            assertIterableEquals(expected, result);
         });
     }
 }
