dd49c979fe574b5e44fc087e0c467422fb2117a9
Oleksandr Roienko <Oleksandr_Roienko@epam.com>
2019-08-07 11:03:49 +0300

    Refactor tests

--- src/test/java/com/waytoodanny/training/students/service/InterviewReadyStudentServiceImplTest.java
+++ src/test/java/com/waytoodanny/training/students/service/InterviewReadyStudentServiceImplTest.java
@@ -4,16 +4,11 @@ import com.waytoodanny.training.students.domain.Student;
 import com.waytoodanny.training.students.domain.filters.CompanyStudentFilter;
 import com.waytoodanny.training.students.domain.filters.PreLabSufficientMarkFilter;
 import com.waytoodanny.training.students.domain.filters.WithinLabSufficientMarkFilter;
-import com.waytoodanny.training.students.repository.CompanyStudentRepository;
 import com.waytoodanny.training.students.repository.CompanyStudentRepositoryImpl;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.lang.reflect.Array;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,23 +28,22 @@ class InterviewReadyStudentServiceImplTest {
     private InterviewReadyStudentServiceImpl interviewReadyStudentService;
 
     @BeforeEach
-    public void init() {
+    void init() {
         MockitoAnnotations.initMocks(this);
         interviewReadyStudentService =
                 Mockito.spy(new InterviewReadyStudentServiceImpl(repository, interviewReadyFilters));
-    }
 
-
-    @Test
-    void shouldReturnAllStudents(){
-        Mockito.when(repository.getAll()).thenReturn(new HashSet<>(Arrays.asList(
+        Mockito.when(repository.getAll()).thenReturn(Arrays.asList(
                 new Student("Masha", 5, 5),
                 new Student("Gena", 2, 3),
                 new Student("Vanya", 3, 4),
                 new Student("Sveta", 5, 5),
                 new Student("Bodya", 3, 3)
-        )));
+        ));
+                doNothing().when(interviewReadyStudentService)
+                        .notifyRemoteServiceAboutGreatStudents(anyCollection());
     }
+
 
     @Test
     void ShouldReturnAllStudentsWithoutAddingFilter() {
@@ -57,7 +51,6 @@ class InterviewReadyStudentServiceImplTest {
             Collection<Student> result = interviewReadyStudentService.getAllReadyForInterview();
             Collection<Student> expected = repository.getAll();
             assertEquals(expected, result);
-            doNothing().when(interviewReadyStudentService).notifyRemoteServiceAboutGreatStudents(anyCollection());
             verify(repository, times(2)).getAll();
         });
 
@@ -68,17 +61,15 @@ class InterviewReadyStudentServiceImplTest {
         interviewReadyFilters.add(new PreLabSufficientMarkFilter());
         interviewReadyFilters.add(new WithinLabSufficientMarkFilter());
 
-        repository = new CompanyStudentRepositoryImpl();
-        interviewReadyStudentService = new InterviewReadyStudentServiceImpl(repository, interviewReadyFilters);
-
-        Collection<Student> result = interviewReadyStudentService.getAllReadyForInterview();
         assertTimeout(Duration.ofMillis(1000), () -> {
-            Collection<Student> expected =  Arrays.asList(
+            Collection<Student> result = interviewReadyStudentService.getAllReadyForInterview();
+            Collection<Student> expected = Arrays.asList(
                     new Student("Sveta", 5, 5),
                     new Student("Vanya", 3, 4),
                     new Student("Masha", 5, 5)
             );
             assertIterableEquals(expected, result);
+            verify(repository, times(1)).getAll();
         });
     }
 }
