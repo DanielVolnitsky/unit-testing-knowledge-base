f2a2d86d3520122b6f42300c130ac980d4e57b2b
Drevish <vladimirnemolovsky@gmail.com>
2019-08-08 18:59:03 +0300

    completed

--- src/test/java/com/waytoodanny/training/calculator/impl/TwoStackCalculatorTest.java
+++ src/test/java/com/waytoodanny/training/calculator/impl/TwoStackCalculatorTest.java
@@ -3,14 +3,90 @@ package com.waytoodanny.training.calculator.impl;
 
 import com.waytoodanny.training.calculator.util.CalculatorUtil;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class TwoStackCalculatorTest {
 
-    private final TwoStackCalculator calculator =
-            new TwoStackCalculator(CalculatorUtil.BASIC_OPERATIONS);
+  private final TwoStackCalculator calculator =
+          new TwoStackCalculator(CalculatorUtil.BASIC_OPERATIONS);
 
-    @Test
-    void calculate() {
+  @Test
+  void shouldAddTwoNumbers() {
+    double result = calculator.calculate("1+1");
+    assertEquals(2, result);
+  }
 
+  @Test
+  void shouldDeductTwoNumbers() {
+    double result = calculator.calculate("10-2");
+    assertEquals(8, result);
+  }
+
+  @Test
+  void shouldDivideTwoNumbers() {
+    double result = calculator.calculate("10/2");
+    assertEquals(5, result);
+  }
+
+  @Test
+  void shouldNotDivideByZero() {
+    try {
+      calculator.calculate("1/0");
+      fail("No division by zero exception");
+    } catch (ArithmeticException e) {
+      assertEquals("/ by zero", e.getMessage());
     }
+  }
+
+  @Test
+  void shouldMultiplyTwoNumbers() {
+    double result = calculator.calculate("2*5");
+    assertEquals(10, result);
+
+    result = calculator.calculate("2*0");
+    assertEquals(0, result);
+  }
+
+  @Test
+  void shouldNotCalculateInvalidOperation() {
+    String operation = "q";
+    try {
+      calculator.calculate(operation);
+      fail("No invalid operation exception");
+    } catch (IllegalArgumentException e) {
+      assertEquals("Invalid operation: " + operation, e.getMessage());
+    }
+  }
+
+  @Test
+  void shouldCalculateWhenSurroundedByParentheses() {
+    double result = calculator.calculate("(2*5)");
+    assertEquals(10, result);
+  }
+
+  @Test
+  void shouldNotCalculateInvalidExpression() {
+    try {
+      calculator.calculate("+");
+      fail("No invalid expression exception");
+    } catch (Exception e) {
+      assertEquals("Invalid expression provided", e.getMessage());
+    }
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+          "(2+4)*5, 30",
+          "2*2*2*2*2, 32",
+          "2/2*2/2, 1",
+          "5*(5-5+1), 5"
+  })
+  void shouldCalculateComplexExpressions(String expression, double expected) {
+    double result = calculator.calculate(expression);
+    assertEquals(expected, result);
+  }
 }
--- src/test/java/com/waytoodanny/training/students/service/InterviewReadyStudentServiceImplTest.java
+++ src/test/java/com/waytoodanny/training/students/service/InterviewReadyStudentServiceImplTest.java
@@ -1,17 +1,73 @@
 package com.waytoodanny.training.students.service;
 
+import com.waytoodanny.training.students.domain.Student;
+import com.waytoodanny.training.students.domain.filters.CompanyStudentFilter;
+import com.waytoodanny.training.students.domain.filters.PreLabSufficientMarkFilter;
+import com.waytoodanny.training.students.domain.filters.WithinLabSufficientMarkFilter;
+import com.waytoodanny.training.students.repository.CompanyStudentRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 
-import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Matchers.anyCollectionOf;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 class InterviewReadyStudentServiceImplTest {
 
-    @Test
-    void getAllReadyForInterview() {
-        assertTimeout(Duration.ofMillis(1000), () -> {
-            //write your test logic here
-        });
-    }
+  private static final List<Student> testStudents = Arrays.asList(
+          new Student("Ollie", 1, 2),
+          new Student("Tyler", 2, 3),
+          new Student("Gabriel", 3, 1),
+          new Student("Michael", 1, 5),
+          new Student("Aarav", 4, 2),
+          new Student("Westin", 5, 4),
+          new Student("Reuben", 3, 3)
+  );
+  private static final List<Student> preLabStudents =
+          Arrays.asList(testStudents.get(4), testStudents.get(5));
+  private static final List<Student> withinLabStudents =
+          Arrays.asList(testStudents.get(3), testStudents.get(5));
+  private static final List<Student> bothFiltersStudents =
+          Arrays.asList(testStudents.get(3), testStudents.get(4), testStudents.get(5));
+
+  private InterviewReadyStudentServiceImpl preLabService;
+  private InterviewReadyStudentServiceImpl withinLabService;
+  private InterviewReadyStudentServiceImpl bothFiltersService;
+
+  @BeforeEach
+  void init() {
+    CompanyStudentFilter preLabFilter = new PreLabSufficientMarkFilter();
+    CompanyStudentFilter withinLabFilter = new WithinLabSufficientMarkFilter();
+
+    MockitoAnnotations.initMocks(this);
+    CompanyStudentRepository repository = mock(CompanyStudentRepository.class);
+    when(repository.getAll()).thenReturn(testStudents);
+
+    preLabService = spy(
+            new InterviewReadyStudentServiceImpl(repository, Collections.singleton(preLabFilter)));
+    withinLabService = spy(
+            new InterviewReadyStudentServiceImpl(repository, Collections.singleton(withinLabFilter)));
+    bothFiltersService = spy(
+            new InterviewReadyStudentServiceImpl(repository, Arrays.asList(preLabFilter, withinLabFilter)));
+
+    doNothing().when(preLabService).notifyRemoteServiceAboutGreatStudents(anyCollectionOf(Student.class));
+    doNothing().when(withinLabService).notifyRemoteServiceAboutGreatStudents(anyCollectionOf(Student.class));
+    doNothing().when(bothFiltersService).notifyRemoteServiceAboutGreatStudents(anyCollectionOf(Student.class));
+  }
+
+  @Test
+  void getAllReadyForInterview() {
+    assertEquals(new HashSet<>(preLabStudents), new HashSet<>(preLabService.getAllReadyForInterview()));
+    assertEquals(new HashSet<>(withinLabStudents), new HashSet<>(withinLabService.getAllReadyForInterview()));
+    assertEquals(new HashSet<>(bothFiltersStudents), new HashSet<>(bothFiltersService.getAllReadyForInterview()));
+  }
 }
