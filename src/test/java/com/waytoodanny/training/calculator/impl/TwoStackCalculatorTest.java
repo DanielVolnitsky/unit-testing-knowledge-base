794cbd28275c4bc187096ed7efd8a369ac290796
Serhii Lytka <Serhii_Lytka@epam.com>
2019-08-13 14:57:10 +0300

    Add tests for TwoStackCalculator and CompanyStudentRepositoryImpl

--- src/test/java/com/waytoodanny/training/calculator/impl/TwoStackCalculatorTest.java
+++ src/test/java/com/waytoodanny/training/calculator/impl/TwoStackCalculatorTest.java
@@ -2,15 +2,39 @@ package com.waytoodanny.training.calculator.impl;
 
 
 import com.waytoodanny.training.calculator.util.CalculatorUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class TwoStackCalculatorTest {
+
+public class TwoStackCalculatorTest {
 
     private final TwoStackCalculator calculator =
             new TwoStackCalculator(CalculatorUtil.BASIC_OPERATIONS);
 
     @Test
-    void calculate() {
+    void addition() {
+        Assertions.assertEquals(3, calculator.calculate("1+2"));
+    }
 
+    @Test
+    void subtraction() {
+        Assertions.assertEquals(-1, calculator.calculate("1-2"));
+    }
+
+    @Test
+    void division() {
+        Assertions.assertEquals(0, calculator.calculate("1/2"));
+    }
+
+    @Test
+    void multiplication() {
+        Assertions.assertEquals(2, calculator.calculate("1*2"));
+    }
+
+    @Test
+    void calculateWithWrongParameter() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> calculator.calculate("String"),
+                "Invalid expression provided");
     }
 }
--- src/test/java/com/waytoodanny/training/students/service/InterviewReadyStudentServiceImplTest.java
+++ src/test/java/com/waytoodanny/training/students/service/InterviewReadyStudentServiceImplTest.java
@@ -1,17 +1,69 @@
 package com.waytoodanny.training.students.service;
 
+import com.waytoodanny.training.students.domain.Student;
+import com.waytoodanny.training.students.domain.filters.CompanyStudentFilter;
+import com.waytoodanny.training.students.domain.filters.PreLabSufficientMarkFilter;
+import com.waytoodanny.training.students.domain.filters.WithinLabSufficientMarkFilter;
+import com.waytoodanny.training.students.repository.CompanyStudentRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.*;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.mockito.ArgumentMatchers.anyCollection;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class InterviewReadyStudentServiceImplTest {
+
+    @Mock
+    private CompanyStudentRepository repository;
+
+    @Spy
+    private Collection<CompanyStudentFilter> interviewReadyFilters = new ArrayList<>(
+            Arrays.asList(new PreLabSufficientMarkFilter(), new WithinLabSufficientMarkFilter()));
+
+    private Collection<Student> allStudents;
+
+    @InjectMocks
+    private InterviewReadyStudentServiceImpl interviewReadyStudentService;
+
+    @BeforeAll
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        allStudents = new ArrayList<>(
+                Arrays.asList(
+                        new Student("Masha", 5, 5),
+                        new Student("Gena", 2, 3),
+                        new Student("Vanya", 3, 4),
+                        new Student("Sveta", 5, 5),
+                        new Student("Bodya", 3, 3)
+                )
+        );
+
+        when(repository.getAll()).thenReturn(allStudents);
+    }
 
     @Test
     void getAllReadyForInterview() {
-        assertTimeout(Duration.ofMillis(1000), () -> {
-            //write your test logic here
-        });
+        InterviewReadyStudentServiceImpl spy = Mockito.spy(new InterviewReadyStudentServiceImpl(repository, interviewReadyFilters));
+        doNothing().when(spy).notifyRemoteServiceAboutGreatStudents(anyCollection());
+
+        assertTimeout(Duration.ofMillis(1000), spy::getAllReadyForInterview);
+    }
+
+    @Test
+    void getAmountOfStudentReadyForInterview() {
+        Assertions.assertEquals(2, interviewReadyStudentService.getAllReadyForInterview().size());
     }
 }
