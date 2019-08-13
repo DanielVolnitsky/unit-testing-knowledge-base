a1424b2eb17f4e326b59cfb4a8eb56f6ccebe22f
Oleksandr Tokarenko <Oleksandr_Tokarenko@epam.com>
2019-08-13 14:52:27 +0300

    Added tests to the methods, test coverage - 82%;

--- src/test/java/com/waytoodanny/training/calculator/impl/TwoStackCalculatorTest.java
+++ src/test/java/com/waytoodanny/training/calculator/impl/TwoStackCalculatorTest.java
@@ -2,15 +2,65 @@ package com.waytoodanny.training.calculator.impl;
 
 
 import com.waytoodanny.training.calculator.util.CalculatorUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class TwoStackCalculatorTest {
+import java.util.NoSuchElementException;
+
+
+public class TwoStackCalculatorTest {
 
     private final TwoStackCalculator calculator =
             new TwoStackCalculator(CalculatorUtil.BASIC_OPERATIONS);
 
     @Test
-    void calculate() {
+    void calculateAdding() {
+        String operation1 = "1+2";
 
+        Assertions.assertEquals(3.0, calculator.calculate(operation1));
+    }
+
+    @Test
+    public void calculateSubtraction() {
+        String operation2 = "5-2";
+
+        Assertions.assertEquals(3, calculator.calculate(operation2));
+    }
+
+    @Test
+    public void calculateMultiply() {
+        String operation4 = "6*2";
+
+        Assertions.assertEquals(12.0, calculator.calculate(operation4));
+    }
+
+    @Test
+    public void calculateDivision() {
+        String operation3 = "6/2";
+
+        Assertions.assertEquals(3.0, calculator.calculate(operation3));
+    }
+
+    @Test
+    public void calculateZeroDivision() {
+        String operation3 = "6/0";
+
+        Assertions.assertThrows(ArithmeticException.class, () -> calculator.calculate(operation3));
+    }
+
+    @Test
+    public void calculateWithIllegalArgumentException() {
+        String operation = "A+10";
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.calculate(operation),
+                "IllegalArgumentException throwed");
+    }
+
+    @Test
+    public void calculateEmptyString() {
+        String emptyString = "";
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> calculator.calculate(emptyString),
+                "NoSuchElementsException throwed");
     }
 }
--- src/test/java/com/waytoodanny/training/students/service/InterviewReadyStudentServiceImplTest.java
+++ src/test/java/com/waytoodanny/training/students/service/InterviewReadyStudentServiceImplTest.java
@@ -1,17 +1,76 @@
 package com.waytoodanny.training.students.service;
 
-import org.junit.jupiter.api.Test;
+import com.waytoodanny.training.students.domain.Student;
+import com.waytoodanny.training.students.domain.filters.CompanyStudentFilter;
+import com.waytoodanny.training.students.domain.filters.PreLabSufficientMarkFilter;
+import com.waytoodanny.training.students.domain.filters.WithinLabSufficientMarkFilter;
+import com.waytoodanny.training.students.repository.CompanyStudentRepository;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
 
 import java.time.Duration;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.mockito.Mockito.*;
 
-class InterviewReadyStudentServiceImplTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class InterviewReadyStudentServiceImplTest {
+
+    @Mock
+    private CompanyStudentRepository repository;
+
+    @Spy
+    private Collection<CompanyStudentFilter> filters = new ArrayList<>(Arrays.asList(new PreLabSufficientMarkFilter(),
+            new WithinLabSufficientMarkFilter()));
+
+    @Spy
+    private Collection<CompanyStudentFilter> emptyFilters;
+
+
+    @InjectMocks
+    private InterviewReadyStudentServiceImpl studentService;
+
+
+
+    private Collection<Student> dataFromRepository;
+
+
+    @BeforeAll
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        dataFromRepository = new HashSet<Student>() {{
+            add(new Student("Alex", 3, 3));
+            add(new Student("Masha", 2, 3));
+            add(new Student("Dima", 3, 3));
+            add(new Student("Vlad", 5, 4));
+        }};
+        when(repository.getAll()).thenReturn(dataFromRepository);
+    }
 
     @Test
     void getAllReadyForInterview() {
+        InterviewReadyStudentServiceImpl spy = spy(new InterviewReadyStudentServiceImpl(repository, filters));
+        doNothing().when(spy).notifyRemoteServiceAboutGreatStudents(anyCollection());
+        Set<Student> temp = new HashSet<Student>() {{
+            add(new Student("Vlad", 5, 4));
+        }};
         assertTimeout(Duration.ofMillis(1000), () -> {
-            //write your test logic here
+            Assertions.assertEquals(temp, spy.getAllReadyForInterview());
         });
+    }
+
+    @Test
+    void getAllReadyForInterviewRetunAllStudents() {
+        InterviewReadyStudentServiceImpl spy = spy(new InterviewReadyStudentServiceImpl(repository, emptyFilters));
+        Set<Student> temp = new HashSet<Student>() {{
+            add(new Student("Alex", 3, 3));
+            add(new Student("Masha", 2, 3));
+            add(new Student("Dima", 3, 3));
+            add(new Student("Vlad", 5, 4));
+        }};
+        Assertions.assertEquals(temp, spy.getAllReadyForInterview());
+
     }
 }
